2c5ee5a4544161cbecbaa409b54d10419ee6efc8
Zeno Rocha <hi@zenorocha.com>
2016-08-18 10:02:40 -0700

    Uses port 80 instead of 3000

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000');
+app.listen(80, function () {
+  console.log('Listening on port 80');
 });
