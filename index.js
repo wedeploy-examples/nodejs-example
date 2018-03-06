7310c1e8b14fb6be3d7d041a8c44e2ea66a67238
Jonni Lundy <jonathan.lundy@liferay.com>
2018-03-06 10:08:45 -0800

    Changes tabs to spaces

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname + '/public/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(80, function () {
